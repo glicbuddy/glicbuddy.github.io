ed8641bba7dd071e212e2f3f42df32ae9d8ae7a4
Caio R. Pereira <caio.ribeiro.pereira@gmail.com>
2026-08-06 20:23:12 -0300

    update modules and added glic notes pdf filename

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -97,6 +97,9 @@ export async function downloadGlicNotesPDF(notes = []) {
 
   stream.on('finish', () => {
     const url = stream.toBlobURL('application/pdf')
-    window.open(url, '_blank')
+    const link = document.createElement('a')
+    link.href = url
+    link.download = 'registros-glicemia.pdf'
+    link.click()
   })
 }
